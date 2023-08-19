a88162df2c85c7790fd464f7210eedd14f5fcdeb
Lan <lan.peng234@gmail.com>
2023-08-19 14:14:01 -0700

    Import Layout from antd

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Layout } from "antd";
+
+const { Header, Content } = Layout;
 
 function App() {
+  // searchNFTs("car");
+  // getNFTTrades("0x745b8158a7e95aaad14e1a58dea21b9f808c93a9");
+  // getContractNFTs("0x745b8158a7e95aaad14e1a58dea21b9f808c93a9");
   return (
     <div className="App">
       <header className="App-header">
