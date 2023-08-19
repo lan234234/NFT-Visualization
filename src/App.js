a11af80cd036c34f969a3708b8fef6649d6cce9c
Lan <lan.peng234@gmail.com>
2023-08-19 14:35:50 -0700

    Add layout of search function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import { useState } from "react";
 import "./App.css";
-import { Layout } from "antd";
+import { Button, Layout, Input } from "antd";
 
 const { Header, Content } = Layout;
 
@@ -7,6 +8,9 @@ function App() {
   // searchNFTs("car");
   // getNFTTrades("0x745b8158a7e95aaad14e1a58dea21b9f808c93a9");
   // getContractNFTs("0x745b8158a7e95aaad14e1a58dea21b9f808c93a9");
+
+  const [searchText, setSearchText] = useState("");
+
   return (
     <Layout Style={{ height: "100vh" }}>
       <Header>
@@ -16,7 +20,17 @@ function App() {
       </Header>
       <Content
         style={{ height: "calc(100% - 64px", padding: 20, overflowY: "auto" }}
-      ></Content>
+      >
+        <Input.Group compact>
+          <Input
+            style={{ width: 500 }}
+            placeholder="Enter a NFT contract address to search"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <Button type="primary">Search</Button>
+        </Input.Group>
+      </Content>
     </Layout>
   );
 }
