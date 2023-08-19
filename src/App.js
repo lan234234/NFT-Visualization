2c1c14b61e2708deb99707f8cdecb9eab1b7c1f9
Lan <lan.peng234@gmail.com>
2023-08-19 14:16:26 -0700

    Add basic JSX in return

--- src/App.js
+++ src/App.js
@@ -9,22 +9,10 @@ function App() {
   // getNFTTrades("0x745b8158a7e95aaad14e1a58dea21b9f808c93a9");
   // getContractNFTs("0x745b8158a7e95aaad14e1a58dea21b9f808c93a9");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout Style={{ height: "100vh" }}>
+      <Header></Header>
+      <Content></Content>
+    </Layout>
   );
 }
 
