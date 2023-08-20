6cc631dbbca651278d361741e7440f2f9acb3948
Lan <lan.peng234@gmail.com>
2023-08-20 14:28:05 -0700

    Add info button NftTransfers

--- src/components/NftTransfers.js
+++ src/components/NftTransfers.js
@@ -1,9 +1,25 @@
+import { Tooltip } from "antd";
+
 const ModalContent = () => {
   return <Table columns={[]} dataSource={[]} pagination={{ pageSize: 5 }} />;
 };
 
-const NftTransfers = () => {
-  return <></>;
+const NftTransfers = ({ nft }) => {
+  constant[(modalOpen, setModalOpen)] = useState(false);
+
+  return (
+    <>
+      <Tooltip title="Transder(s) on this NFT">
+        <Button
+          style={{ border: "none" }}
+          size="large"
+          shape="circle"
+          icon={<InfoCircleOutlined />}
+          onClick={() => setModalOpen(true)}
+        />
+      </Tooltip>
+    </>
+  );
 };
 
 export default NftTransfers;
