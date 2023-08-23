bdc761c2f7b155ba75e15a406074a0b7cf3390ee
lan234234 <104956570+lan234234@users.noreply.github.com>
2023-08-22 19:23:19 -0700

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 const origin = "https://deep-index.moralis.io";
 const apiKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjYwMTE0YmIzLWQyMWYtNDVmOC1iYjJlLTMyMDU0ZWI3MDcwNCIsIm9yZ0lkIjoiMzUzODMyIiwidXNlcklkIjoiMzYzNjYyIiwidHlwZUlkIjoiOTE2Zjc5NTUtOWQ4MS00ZjMzLTliZmMtMzM1YzM1NDQ0YmJiIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE2OTI0NjQxOTYsImV4cCI6NDg0ODIyNDE5Nn0.jziHJ7hwDEMJgeJwe5X7zL0QPFD67Y89yBdR3aQa3BU";
+  "your moralis apiKey";
 
 export const getContractNFTs = async (tokenAddress) => {
   const url = new URL(`${origin}/api/v2/nft/${tokenAddress}`);
